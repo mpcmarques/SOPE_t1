aa17a15cfb80138b1cbbba9e9aeb344fd7d43e18
mpcmarques <mpcmarquess@icloud.com>
2017-04-09 21:45:51 +0100

    teste

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # sfind
+
+
+Criado por Mateus Pedroza
+Programa imita a função find do linux.
+
+
+Exemplos de invocação com find:
+
+shell> find ~ -name lixo.txt -print
+/home/user/tmp/lixo.txt
+/home/user/mbin/myscanf/tmp/lixo.txt
+
+shell> find ~ -name lixo.txt -exec file '{}' \;  
+/home/user/tmp/lixo.txt: ASCII text
+/home/user/mbin/myscanf/tmp/lixo.txt: HTML document text  
+
+shell> find ~ -name lixo.txt -delete
+shell>
+
+shell> find . -type l -print
+./so/www
+./so/material
+...
+
+shell> find / -perm 0400 -print
+/bak/multimedia/figs/bear.jpg
+/bak/multimedia/figs/cubs.jpg
--- main.c
+++ main.c
@@ -6,127 +6,68 @@
 //  Copyright © 2017 Mateus Pedroza. All rights reserved.
 //
 
-#include <stdio.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <unistd.h>
-#include <string.h>
-#include <unistd.h>
-#include <pwd.h>
 
-/** 
- Gets default path
- @param path  Empty char vector that will be filled with path;
- */
-void getDefaultPath(char path[]){
-    char pathname[1024];
-    
-    if (getcwd(pathname, sizeof(pathname)) != NULL){
-        strcpy(path, pathname);
-    }else
-        perror("getcwd() error");
-}
+#include "options.h"
+#include "sfind.h"
 
-/** 
- *  Founded file action handler
- */
-void handleFoundFile(struct dirent dirent, char path[], const char command[]){
-    char newPath[1024];
-    
-    //  Create new path
-    strcpy(newPath, path);
-    strcat(newPath, "/");
-    strcat(newPath, dirent.d_name);
-    
-    if (strcmp(command, "-print") == 0){
-        printf("%s\n", newPath);
-    }
-    else if (strcmp(command, "-delete") == 0){
-        remove(newPath);
-    }
-}
+void parseArguments(char *argv[]);
 
-int sfind(const char searchedText[], char pathname[], const char command[]){
-    struct dirent *dirent = NULL;
-    
-    //  Open directory
-    DIR *directory = opendir(pathname);
-    
-    if (directory != NULL)
-    { // printf("Directory opened\n");
+  int main(int argc, char *argv[]) {
+
+    //  Check for invalid arguments
+    if (argc < 4) {
+      printf("Usage:\n sfind [ '.', '/', '~' ] ['-name', '-type', '-perm'] [file] ['-print', '-delete', '-exec']\n");
+      exit(1);
+    } else {
+      parseArguments(argv);
     }
-    else
-        perror("opendir() error");
-    
-    //  Read directory file
-    
-    while ((dirent = readdir(directory))) {
-        
-        //  Check if dirent is a directory, and is diferent from "." and ".."
-        if (dirent->d_type == DT_DIR &&
-            strcmp(dirent->d_name, "..") != 0 &&
-            strcmp(dirent->d_name, ".") != 0){
-            //  Found a directory, search in directory
-            char newPath[1024];
-            
-            //  Create new path
-            strcpy(newPath, pathname);
-            strcat(newPath, "/");
-            strcat(newPath, dirent->d_name);
-            
-            //  Search in new path recursively
-            sfind(searchedText, newPath, command);
-            
+  }
+
+
+  void parseArguments(char *argv[]){
+        //  arguments to parse
+        char *searchDirectory;
+        char *searchParameter;
+        char *fileName;
+        char *command;
+
+        //  Directory '.','/','~'
+        if ((searchDirectory = argv[1]) == NULL) {
+          /* code */
         }
-        
-        //  If dirent name is equal searched text, print path
-        if (strcmp(dirent->d_name, searchedText) == 0) {
-            handleFoundFile(*dirent, pathname, command);
+        else if (strcmp(searchDirectory, DIR_CURRENT) == 0){
+
         }
-    }
-    
-    
-    closedir(directory);
-    return 1;
-}
+        else if (strcmp(searchDirectory, DIR_HOME) == 0){
 
-/**
- * Return home path
- */
+        }
+        else if (strcmp(searchDirectory, DIR_ROOT) == 0){
 
-void getHomePath(char pathname[]){
-    struct passwd *info = getpwuid(getuid());
-    
-    strcpy(pathname, info->pw_dir);
-}
+        }
 
+        //  Search parameter '-name', '-type', '-perm'
+        if ((searchParameter = argv[2]) == NULL) {
+          /* code */
+        }
+        else if (strcmp(searchParameter, PARAM_NAME) == 0){
 
+        }
+        else if (strcmp(searchParameter, PARAM_TYPE) == 0){
 
+        }
+        else if (strcmp(searchParameter, PARAM_PERM) == 0){
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    
-    char pathname[1024];
-    const char *searchType = argv[1];
-    const char *searchParameter = argv[2];
-    const char *fileName = argv[3];
-    const char *command = argv[4];
-    
-    // searchtypes possibles : . ~ /
-    // .
-    if (strcmp(searchType, ".") == 0 ){
-        getDefaultPath(pathname);
-    }
-    else if(strcmp(searchType, "~") == 0){
-        getHomePath(pathname);
-    }
-    
-    //  searchparameters possibles : -name -type -perm
-    
-    //  commands possible: -delete -read
-    printf("%s\n", pathname);
-    sfind(fileName, pathname, command);
-    
-    return 0;
-}
+        }
+
+        //  Command parameter '-print', '-delete', '-exec'
+        if ((command = argv[4]) == NULL) {
+          /* code */
+        }
+        else if (strcmp(command, CMD_PRINT) == 0){
+
+        }
+        else if (strcmp(command, CMD_DELETE) == 0){
+
+        }
+        else if (strcmp(command, CMD_EXECUTE) == 0){}
+  }
